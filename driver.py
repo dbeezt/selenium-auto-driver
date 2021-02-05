853306b8687d93798fb630d9fe628237672df711
dbeezt <dbrereton1995@gmail.com>
2021-02-05 22:41:42 +0000

    Added check to deduce browser

--- driver.py
+++ driver.py
@@ -1,9 +1,12 @@
 import platform
+import webbrowser
 
+class Driver():
+    example_browsers = ['firefox', 'chromium', 'chrome']
 
-class  Driver():
-    def __init__(self, **args):
+    def __init__(self, browsers = example_browsers):
         self.os = self.which_os()
+        self.browser = self.detect_browser(browsers = browsers)
 
     def which_os(self):
         os = platform.system()
@@ -14,15 +17,29 @@ class  Driver():
                 'Darwin': 'mac'
             }.get(os, None)
         return map_os(os)
+
+    def detect_browser(self, browsers):
+        for browser in browsers:
+            try:
+                webbrowser.get(using = browser)
+            except Exception as e:
+                print(f"Webbrowser Error: {e}")
+            else:
+                return browser
+        
+        return None
         
 
 class Gecko(Driver):
     def __init__(self, **args):
         super().__init__()
-        print(self.os)
+        print(self.browser)
 
 
 class Chromium(Driver):
     def __init__(self, **args):
         super().__init__()
         print(self.os)
+
+
+Gecko()
